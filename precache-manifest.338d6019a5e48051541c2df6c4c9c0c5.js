41b2b882c8cb3324911d695db1134ded03bd5b52
Uy Binh <uybinh@gmail.com>
2018-10-26 15:58:49 +0700

    Updates

---
+++ precache-manifest.338d6019a5e48051541c2df6c4c9c0c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a238d092a82fb169ff7",
+    "url": "/react-croppie/static/js/runtime~main.2a238d09.js"
+  },
+  {
+    "revision": "790151356fdb47318fee",
+    "url": "/react-croppie/static/js/main.79015135.chunk.js"
+  },
+  {
+    "revision": "36eb039264702eb883f1",
+    "url": "/react-croppie/static/js/1.36eb0392.chunk.js"
+  },
+  {
+    "revision": "790151356fdb47318fee",
+    "url": "/react-croppie/static/css/main.31f63146.chunk.css"
+  },
+  {
+    "revision": "36eb039264702eb883f1",
+    "url": "/react-croppie/static/css/1.3d29f992.chunk.css"
+  },
+  {
+    "revision": "c1f14ffbd42b1142da9ea44ba83d1f61",
+    "url": "/react-croppie/index.html"
+  }
+];
